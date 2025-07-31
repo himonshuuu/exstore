d4727a37bb2e3ee2825a1b8de905d2d752ab0e9a
himonshuuu <saikiahimangshu1256@gmail.com>
2025-07-31 18:31:58 +0530

    Enhance ExStore with new operations including batch put/get, key existence check, and array manipulations. Introduce request recording for recovery engine.

--- lib/exstore.ex
+++ lib/exstore.ex
@@ -1,6 +1,6 @@
 defmodule ExStore do
   @moduledoc """
-  A simple in-memory key-value store.
+  An enhanced in-memory key-value store with file-based persistence, sharding, and advanced operations.
   """
 
   @server ExStore.Cache.Cache
@@ -19,21 +19,56 @@ defmodule ExStore do
   Sets the value of `key` to `value` with an optional time-to-live (TTL) in seconds.
   """
   def put(key, value, ttl \\ nil) do
+    ExStore.RecoveryEngine.record_request(:set, key, value)
     GenServer.call(@server, {:set, key, value, ttl})
+  end
+
+  @doc """
+  Sets multiple key-value pairs in a single operation.
+  """
+  def put_many(data) do
+    ExStore.RecoveryEngine.record_request(:set_many, nil, data)
+    GenServer.call(@server, {:set_many, data})
   end
 
   @doc """
   Retrieves the value associated with `key`.
   """
   def get(key) do
+    ExStore.RecoveryEngine.record_request(:get, key)
     GenServer.call(@server, {:get, key})
+  end
+
+  @doc """
+  Retrieves multiple values by their keys.
+  """
+  def get_many(keys) do
+    ExStore.RecoveryEngine.record_request(:get_many, keys)
+    GenServer.call(@server, {:get_many, keys})
+  end
+
+  @doc """
+  Checks if a key exists in the cache.
+  """
+  def has(key) do
+    ExStore.RecoveryEngine.record_request(:has, key)
+    ExStore.Cache.Cache.has(@server, key)
   end
 
   @doc """
   Deletes the key-value pair associated with `key`.
   """
   def delete(key) do
+    ExStore.RecoveryEngine.record_request(:delete, key)
     GenServer.call(@server, {:delete, key})
+  end
+
+  @doc """
+  Deletes multiple keys in a single operation.
+  """
+  def delete_many(keys) do
+    ExStore.RecoveryEngine.record_request(:delete_many, keys)
+    GenServer.call(@server, {:delete_many, keys})
   end
 
   @doc """
@@ -42,5 +77,58 @@ defmodule ExStore do
   def ttl(key) do
     GenServer.call(@server, {:ttl, key})
   end
-end
 
+  @doc """
+  Returns all key-value pairs in the cache.
+  """
+  def all do
+    ExStore.RecoveryEngine.record_request(:all)
+    ExStore.Cache.Cache.dump_all()
+  end
+
+  # Array operations
+  @doc """
+  Pushes a value to the end of an array stored at the given key.
+  """
+  def push(key, value) do
+    ExStore.RecoveryEngine.record_request(:set, key, value)
+    ExStore.Cache.Cache.push(@server, key, value)
+  end
+
+  @doc """
+  Pops a value from the end of an array stored at the given key.
+  """
+  def pop(key) do
+    result = ExStore.Cache.Cache.pop(@server, key)
+    if result.element != nil do
+      ExStore.RecoveryEngine.record_request(:set, key, result)
+    end
+    result
+  end
+
+  @doc """
+  Shifts a value from the beginning of an array stored at the given key.
+  """
+  def shift(key) do
+    result = ExStore.Cache.Cache.shift(@server, key)
+    if result.element != nil do
+      ExStore.RecoveryEngine.record_request(:set, key, result)
+    end
+    result
+  end
+
+  @doc """
+  Unshifts a value to the beginning of an array stored at the given key.
+  """
+  def unshift(key, value) do
+    ExStore.RecoveryEngine.record_request(:set, key, value)
+    ExStore.Cache.Cache.unshift(@server, key, value)
+  end
+
+  @doc """
+  Slices an array stored at the given key.
+  """
+  def slice(key, start, stop \\ nil) do
+    ExStore.Cache.Cache.slice(@server, key, start, stop)
+  end
+end
